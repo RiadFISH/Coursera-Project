db82544a36d1deffb50c78f946d87abd055c49fc
RiadFISH <riad.ipsc@gmail.com>
2020-05-21 20:03:32 +0600

    First

---
+++ Loop.R
@@ -0,0 +1,27 @@
+makeVector <- function(x = vector()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinversen = setinverse,
+       getinverse = getinverse)
+}
+
+#cachemen
+
+cachemean <- function(x, ...) {
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- inverse(data, ...)
+  x$setinverse(i)
+  i
+}
